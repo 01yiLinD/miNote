72d0908467e535a4d1d181ed294320632b79e4db
Rui <01foreverbob@gmail.com>
2025-03-24 16:29:20 +0800

    started

---
+++ app/build.gradle.kts
@@ -0,0 +1,60 @@
+import com.android.build.gradle.internal.utils.restrictRenderScriptOnRiscv
+
+plugins {
+    alias(libs.plugins.android.application)
+}
+
+android {
+    namespace = "net.micode.notes"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "net.micode.notes"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    packaging {
+        resources.excludes.add("META-INF/DEPENDENCIES");
+        resources.excludes.add("META-INF/NOTICE");
+        resources.excludes.add("META-INF/LICENSE");
+        resources.excludes.add("META-INF/LICENSE.txt");
+        resources.excludes.add("META-INF/NOTICE.txt");
+    }
+}
+
+dependencies {
+
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.activity)
+    implementation(libs.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+//    implementation(fileTree(mapOf(
+//        "dir" to "libs",
+//        "include" to listOf("*.aar", "*.jar"),
+//        "exclude" to emptyList<String>()
+//    )))
+    implementation(files("libs/httpclient-osgi-4.5.14.jar"))
+    implementation(files("libs/httpclient-win-4.5.14.jar"))
+    implementation(files("libs/httpcore-4.4.16.jar"))
+}
